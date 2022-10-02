fe5efe6f9bb2d54f26a87b979f9a549f64890617
Yamamoto Tatsuya <43805056+yamatatsu10969@users.noreply.github.com>
2022-10-01 20:21:16 -0700

    Add base ui

--- lib/presentation/pages/accounts/accounts_page.dart
+++ lib/presentation/pages/accounts/accounts_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:phr_app_expo/presentation/styles/design_size.dart';
 import 'package:phr_app_expo/presentation/widgets/tap_to_unfocus_view.dart';
@@ -14,14 +15,45 @@ class AccountsPage extends HookConsumerWidget {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.stretch,
             mainAxisAlignment: MainAxisAlignment.center,
-            children: const [
-              Center(
-                child: Text('Accounts'),
+            children: [
+              rowItem(
+                const Text('Image'),
+                const Text('ONE OK ROCK'),
               ),
+              rowItem(
+                IconButton(
+                  icon: const FaIcon(FontAwesomeIcons.instagram),
+                  onPressed: () {},
+                ),
+                const Text('Image'),
+              ),
+              rowItem(
+                const Text('Image'),
+                const Text('TORU'),
+              ),
+              rowItem(
+                const Text('RYOTA'),
+                const Text('Image'),
+              ),
+              rowItem(
+                const Text('Image'),
+                const Text('TOMOYA'),
+              )
             ],
           ),
         ),
       ),
+    );
+  }
+
+  Widget rowItem(Widget left, Widget right) {
+    return Row(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Expanded(child: Center(child: left)),
+        Expanded(child: Center(child: right)),
+      ],
     );
   }
 }
